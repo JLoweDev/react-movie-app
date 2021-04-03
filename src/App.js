b1062d86460451ccade3553dfa141ce3b01a3afe
James <67014970+JLoweDev@users.noreply.github.com>
2021-04-03 23:35:54 +0100

    added APIs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ console.log(process.env.REACT_APP_API_KEY)
 
 // APIs
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popular.desc&api_key=${process.env.REACT_APP_API_KEY}&page=1`;
+const IMAGES_API = `https://image.tmdb.org/t/p/w1280`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=`
 
 function App() {
   const movies = ["1", "2", "3"];
