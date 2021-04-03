c071e961c2cc39534dae8d74f97d298f66e8a32d
James <67014970+JLoweDev@users.noreply.github.com>
2021-04-03 18:31:54 +0100

    dotenv added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Movie from './components/Movie'
+require('dotenv').config();
+
+
+// APIs
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popular.desc&api_key=${process.env.API_KEY}&page=1`;
 
 function App() {
   const movies = ["1", "2", "3"];
 
   return (
     <div className="App">
+      <p>{FEATURED_API}</p>
       {movies.map((movie) => (
         <Movie />
       ))}
