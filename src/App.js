3a5fa153ca6e99a1d455df94a9a64ebcaffe2126
James <67014970+JLoweDev@users.noreply.github.com>
2021-04-03 23:50:49 +0100

    data brought in from API featured useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import Movie from './components/Movie'
-console.log(process.env.REACT_APP_API_KEY)
+import { useEffect, useState } from 'react';
+import Movie from './components/Movie';
 
 
 // APIs
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popular.desc&api_key=${process.env.REACT_APP_API_KEY}&page=1`;
 const IMAGES_API = `https://image.tmdb.org/t/p/w1280`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=`
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=`;
 
 function App() {
-  const movies = ["1", "2", "3"];
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API).then((res) => res.json()).then((data) => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  }, [])
 
   return (
     <div className="App">
-      <p>{FEATURED_API}</p>
       {movies.map((movie) => (
-        <Movie />
+        <Movie key={movie.id} data={movie}/>
       ))}
     </div>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,10 @@
-const Movie = () => {
+const IMAGES_API = `https://image.tmdb.org/t/p/w1280`;
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
     return (
         <div className='movie'>
             This is a movie component
+            
         </div>
     )
 }
